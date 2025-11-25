d3a76361d9d3b8e6c9c370dac67196e59d687f0e
mcdssd <mcs56@discente.ifpe.edu.br>
2025-11-24 21:43:24 -0300

    [Assets]: add background

--- app/src/main/java/com/example/nadanao/MainActivity.kt
+++ app/src/main/java/com/example/nadanao/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nadanao.ui.theme.NadaNaoTheme
+import androidx.compose.foundation.background
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +26,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             NadaNaoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TelaComBackground(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun TelaComBackground(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Transparent)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        // Conteúdo por cima do background
+        Greeting(
+            name = "Android",
+            modifier = modifier
+        )
     }
 }
 
@@ -42,6 +69,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     NadaNaoTheme {
-        Greeting("Android")
+        TelaComBackground()
     }
 }
